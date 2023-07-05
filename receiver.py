86a08df3f0a2873439de683383161f8c5df2caea
Nitish SJSU <nitishSJSU@Nitishs-MacBook-Air.local>
2023-07-05 12:37:21 -0700

    Receiver loop+buffer size test

--- receiver.py
+++ receiver.py
@@ -1,6 +1,7 @@
 import socket
 
-RECEIVER_IP = socket.gethostname()
+# RECEIVER_IP = socket.gethostname()
+RECEIVER_IP = '10.0.0.211'
 RECEIVER_PORT = 1024
 
 
@@ -25,24 +26,32 @@ def start_server():
 
     # Send a response back to the client
     response = "Hello, sender! Successfully received message"
-    client_socket.sendall(response.encode('utf-8'))
+    client_socket.send(response.encode('utf-8'))
 
     if data == "Hello, from Project Sender!":
-        return True, receiver_socket
+        return True, client_socket
     return False, None
 
-
-if __name__ == "__main__":
-    accept_messages = True
+def receive_messages(is_success, socket_conn):
+    accept_messages_flag = True
     received_messages = {}
-    is_success, socket_conn = start_server()
-    while accept_messages:
-        data = socket_conn.recv(1024).decode('utf-8')
-        if data and data.startswith('TERMINATE'):
-            accept_messages = False
+    while accept_messages_flag:
+        data = socket_conn.recv(4).decode('utf-8')
+        data = data.strip()
+        print(data.strip())
+        if data and data.startswith('T'):
+            accept_messages_flag = False
+            print('got terminate msg')
+            socket_conn.close()
         elif data:
             churn = data.split(':')
             seq_num, message = churn[0], churn[1]
             received_messages[seq_num] = message
     print(received_messages)
-    socket_conn.close()
+    # socket_conn.close()
+
+
+if __name__ == "__main__":
+    is_success, socket_conn = start_server()
+    receive_messages(is_success, socket_conn)
+    
